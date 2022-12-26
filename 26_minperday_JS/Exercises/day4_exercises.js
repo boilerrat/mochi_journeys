67b3b98116e42c62f004d2245521bcff9e3c62e7
boilerrat <128boilerrat@gmail.com>
2022-12-26 14:28:34 -0500

    day 4 exercises

--- 26_minperday_JS/Exercises/day4_exercises.js
+++ 26_minperday_JS/Exercises/day4_exercises.js
@@ -46,3 +46,150 @@ else if (myAgeInt < yourAgeInt) {
 else if (myAgeInt == yourAgeInt) {
     console.log('We are the same age');
 }
+
+// Question 3
+
+/*
+If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
+
+    using if else
+    ternary operator.
+
+      let a = 4
+      let b = 3
+
+        4 is greater than 3
+*/
+// first way
+
+let a = prompt('Enter a number', 'number');
+let aInt = parseInt(a);
+let b = prompt('Enter a number', 'number');
+let bInt = parseInt(b);
+if (aInt > bInt) {
+    console.log("A is Greater than B")
+}
+else {
+    console.log('A is less than B');
+}
+
+// second way
+
+let aA = prompt('Enter a number', 'number');
+let aAInt = parseInt(aA);
+let bB = prompt('Enter a number', 'number');
+let bBInt = parseInt(bB);
+const result = aAInt > bBInt
+    ? 'A is Greater than B'
+    : 'A is less than B';
+console.log(result);
+
+// Question 4
+
+/*
+Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript? 
+
+*/
+
+let number = prompt('Enter a number', 'number');
+let numberInt = parseInt(number);
+if (numberInt % 2 === 0) {
+    console.log('Number is even');
+}
+else {
+    console.log('Number is odd');
+}
+
+
+// Exercises Level 2
+/* Question 1
+Write a code which can give grades to students according to their scores:
+
+    80-100, A
+    70-89, B
+    60-69, C
+    50-59, D
+    0-49, F
+*/
+let grade = prompt('Enter a number', 'number');
+let gradeInt = parseInt(grade);
+switch(true) {
+    case (gradeInt >= 80 && gradeInt <= 100):
+        console.log('Your Grade is A');
+        break;
+    case (gradeInt >= 70 && gradeInt <= 79):
+        console.log('Your grade is B');
+        break;
+    case (gradeInt >= 60 && gradeInt <= 69):
+        console.log('Your Grade is C');
+        break;
+    case (gradeInt >= 50 && gradeInt <= 59):
+        console.log('Your Grade is D');
+        break;
+    case (gradeInt >= 0 && gradeInt <= 49):
+        console.log('You Failed');
+        break;
+    default:
+        console.log('Entered value was not a valid grade');
+}
+
+
+// Question 2
+
+/*
+Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+
+    September, October or November, the season is Autumn.
+    December, January or February, the season is Winter.
+    March, April or May, the season is Spring
+    June, July or August, the season is Summer
+*/
+
+
+let monthInput = prompt('Enter a month', 'month');
+let month = monthInput.toLowerCase();
+switch (month) {
+    case 'september':
+    case 'october':
+    case 'november':
+        console.log('The season is Autumn');
+        break;
+    case 'december':
+    case 'january':
+    case 'february':
+        console.log('The season is Winter');
+        break;
+    case 'march':
+    case 'april':
+    case 'may':
+        console.log('The season is Spring');
+        break;
+    case 'june':
+    case 'july':
+    case 'august':
+        console.log('The season is Summer');
+        break;
+    default:
+        console.log('Enter a valid month');
+}
+                  
+// Question 3
+
+/* 
+Check if a day is weekend day or a working day. Your script will take day as an input.
+
+    What is the day  today? Saturday
+    Saturday is a weekend.
+
+    What is the day today? saturDaY
+    Saturday is a weekend.
+
+    What is the day today? Friday
+    Friday is a working day.
+
+    What is the day today? FrIDAy
+    Friday is a working day.
+
+ */
+
+    
