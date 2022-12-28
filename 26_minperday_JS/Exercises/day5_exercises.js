409534485d0d3da31e72642d6d06f86a78320348
boilerrat <128boilerrat@gmail.com>
2022-12-27 20:43:04 -0500

    day 5 exercises

---
+++ 26_minperday_JS/Exercises/day5_exercises.js
@@ -0,0 +1,55 @@
+/*
+30 Days of Javascript
+
+Day 5 - Arrays
+
+*/
+
+/*
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'
+]
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+]
+*/
+
+// Question 1
+// Declare an empty array;
+
+const emptyArray = [];
+
+// or
+
+const emptyArr = Array();
+
+// or
+
+let emptyArr2 = new Array();
+
+console.log(emptyArray);
+console.log(emptyArr);
+console.log(emptyArr2);
+
+// Question 2
+// Declare an array with more than 5 number of elemnents
+
+const moreThanFive = [1, 2, 3, 4, 5, 6]
+console.log(moreThanFive);
